a4f48a853ba2afb16d84fb348272e448ab94480a
Mohammed Hossain <jahangir7373hossain@gmail.com>
2021-07-18 14:26:23 -0400

    initial commit

---
+++ src/main/java/base/Driver.java
@@ -0,0 +1,34 @@
+package base;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import utils.TestUtils;
+
+public class Driver {
+
+
+	public static WebDriver driver;
+		
+	public static void setDriver() {
+		String browser = TestUtils.getProperty("browser");
+		if(browser.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "./src/main/resources/exe_files/chromedriver.exe");
+			driver = new ChromeDriver();
+		}else if(browser.equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.gecko.driver", "./src/main/resources/exe_files/geckodriver.exe");
+			driver = new FirefoxDriver();
+		}else if(browser.equalsIgnoreCase("ie")) {
+			System.setProperty("webdriver.ie.driver", "./src/main/resources/exe_files/IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+		}
+		driver.manage().window().maximize();
+		driver.get(TestUtils.getProperty("appUrl"));
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);		
+	}
+
+}
