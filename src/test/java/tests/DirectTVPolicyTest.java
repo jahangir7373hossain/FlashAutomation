1d6ab832da5208f971c69d49b857ea792474bad2
Mohammed Hossain <jahangir7373hossain@gmail.com>
2021-07-27 17:30:50 -0400

    change

--- src/test/java/tests/DirectTVPolicyTest.java
+++ src/test/java/tests/DirectTVPolicyTest.java
@@ -1,13 +1,29 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import base.TestBase;
+import utils.TestUtils;
 
 public class DirectTVPolicyTest extends TestBase{
+	
+	TestUtils ts = new TestUtils();
 
 	@Test
 	public void testingUrl () {
 		
+		String [] actual = TestUtils.getProperty("name").split(" ");
+		String [] expected = directTvPolicyPage.PolicyPageElement.getText().split(" ");
+		
+		for(int i = 0; i < actual.length; i++) {
+			
+			Assert.assertEquals(actual, expected);
+		}
+		
+	}
+	
+	public void gitTest() {
+		// this is a testing method for git
 	}
 }
